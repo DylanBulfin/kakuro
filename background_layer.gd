6ca28eb8e963afff3045e1a4fd1800c73ba5ccf9
Dylan Bulfin <dylanbulfin@gmail.com>
2025-03-20 19:04:05 -0400

    Add selection

--- background_layer.gd
+++ background_layer.gd
@@ -1,5 +1,7 @@
 extends TileMapLayer
 
+var selected: Vector2i
+
 func draw_puzzle(puzzle: Puzzle) -> void:
 	for x in range(puzzle.width):
 		for y in range(puzzle.height):
@@ -12,3 +14,8 @@ func draw_puzzle(puzzle: Puzzle) -> void:
 			elif len(cell.rules) == 1:
 				set_cell(Vector2i(x, y), 0, Vector2i(4, 0))
 			
+func change_selection(row: int, column: int) -> void:
+	if selected:
+		set_cell(selected, 0, Vector2i(1, 0))
+	selected = Vector2i(column, row)
+	set_cell(selected, 0, Vector2i(2, 0))
--- kakuro.gd
+++ kakuro.gd
@@ -1,11 +1,12 @@
 extends Node
 
 @onready var puzzle: Puzzle
+var selected: Cell
 
 func _ready() -> void:
 	var rules: Array[Rule] = [
-		Rule.new(1, 1, 2, 45, true),
-		Rule.new(1, 1, 2, 32, false),
+		Rule.new(1, 1, 2, 17, true),
+		Rule.new(1, 1, 2, 16, false),
 		Rule.new(0, 2, 3, 12, true),
 		Rule.new(2, 0, 3, 12, false),
 		Rule.new(0, 3, 3, 10, true),
@@ -15,3 +16,22 @@ func _ready() -> void:
 	puzzle = Puzzle.new(4, 4, rules)
 	%BackgroundLayer.draw_puzzle(puzzle)
 	%Rules.draw_puzzle(puzzle)
+	
+	# Find first selectable entry
+	var first_entry: Cell
+	for cell in puzzle.rows[1]:
+		if cell.fillable:
+			first_entry = cell
+			break
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		var tile_pos: Vector2i = event.position / 32
+		change_selection(tile_pos.y, tile_pos.x)
+		
+
+func change_selection(row: int, column: int) -> void:
+	var cell: Cell = puzzle.rows[row][column]
+	if cell.fillable:
+		selected = cell
+		%BackgroundLayer.change_selection(row, column)
